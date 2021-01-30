d0f68cd59e8bb122290fa6f9cb188c73ca3f39c7
Lassi Haasio <ilikeavocadoes@hush.com>
2021-01-30 17:55:36 +0200

    Add rofi as a dependency in derivation.nix

--- derivation.nix
+++ derivation.nix
@@ -1,8 +1,11 @@
-{ stdenv, libXft, libXinerama }:
+{ stdenv, libXft, libXinerama, rofi }:
 
 stdenv.mkDerivation rec {
   name = "dwm";
   src = ./.;
   buildInputs = [ libXft libXinerama ];
-  prePatch = ''sed -i "s:/usr/local:$out:" config.mk'';
+  prePatch = ''
+    sed -i "s:/usr/local:$out:" config.mk
+    substituteInPlace config.def.h --replace "rofi" "${rofi}/bin/rofi"
+  '';
 }
